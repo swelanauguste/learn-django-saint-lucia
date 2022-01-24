26a2f4d485727dfd05cae7b68ef1b9d1159c3d9d
kingship <swelanauguste@gmail.com>
2022-01-24 17:04:29 +0000

    added markdown features

--- app/lessons/models.py
+++ app/lessons/models.py
@@ -3,18 +3,31 @@ from django.urls import reverse
 from django.utils.text import slugify
 from django_resized import ResizedImageField
 
+class PublishedLessonManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status='published')
+
 
 class Lesson(models.Model):
+    LESSON_STATUSES = [
+        ("draft", "Draft"),
+        ("published", "Published"),
+    ]
+    status = models.CharField(max_length=25, choices=LESSON_STATUSES, default='draft')
     title = models.CharField(max_length=255, unique=True)
     slug = models.SlugField(max_length=255, unique=True, blank=True)
     description = models.TextField(blank=True)
+    instructions = models.TextField(blank=True)
     image = ResizedImageField(size=[500, 300], upload_to='lesson_images', blank=True, null=True, default='img/lesson_default.png')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    objects = models.Manager()
+    published_objects = PublishedLessonManager()
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify(self.title + "-" + str(self.updated_at))
+            self.slug = slugify(self.title)
         super(Lesson, self).save(*args, **kwargs)
         
     def get_absolute_url(self):
--- app/lessons/views.py
+++ app/lessons/views.py
@@ -6,6 +6,11 @@ from .models import Lesson
 
 class LessonListView(ListView):
     model = Lesson
+    
+    def get_queryset(self):
+        queryset = Lesson.published_objects.all()
+        return queryset
+    
 
 
 class LessonDetailView(DetailView):
