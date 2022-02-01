b0465e303e737bbdc6be33060adc7346e35dafa2
kingship <swelanauguste@gmail.com>
2022-02-01 05:16:17 +0000

    added hitcount

--- app/lessons/models.py
+++ app/lessons/models.py
@@ -2,26 +2,35 @@ from django.db import models
 from django.urls import reverse
 from django.utils.text import slugify
 from django_resized import ResizedImageField
+from hitcount.models import HitCountMixin
+
 
 class PublishedLessonManager(models.Manager):
     def get_queryset(self):
-        return super().get_queryset().filter(status='published')
+        return super().get_queryset().filter(status="published")
 
 
-class Lesson(models.Model):
+class Lesson(HitCountMixin, models.Model):
     LESSON_STATUSES = [
         ("draft", "Draft"),
         ("published", "Published"),
     ]
-    status = models.CharField(max_length=25, choices=LESSON_STATUSES, default='draft')
+    status = models.CharField(max_length=25, choices=LESSON_STATUSES, default="draft")
     title = models.CharField(max_length=255, unique=True)
     slug = models.SlugField(max_length=255, unique=True, blank=True)
     description = models.TextField(blank=True)
     instructions = models.TextField(blank=True)
-    image = ResizedImageField(size=[500, 300], upload_to='lesson_images', blank=True, null=True, default='img/lesson_default.png')
+    image = ResizedImageField(
+        size=[500, 300],
+        upload_to="lesson_images",
+        blank=True,
+        null=True,
+        default="img/lesson_default.png",
+    )
+    
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
     objects = models.Manager()
     published_objects = PublishedLessonManager()
 
@@ -29,7 +38,7 @@ class Lesson(models.Model):
         if not self.slug:
             self.slug = slugify(self.title)
         super(Lesson, self).save(*args, **kwargs)
-        
+
     def get_absolute_url(self):
         return reverse("lessons:lesson-detail", kwargs={"slug": self.slug})
 
--- app/lessons/views.py
+++ app/lessons/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from hitcount.views import HitCountDetailView, HitCountMixin
 
 from .models import Lesson
 
 
-class LessonListView(ListView):
+class LessonListView(HitCountMixin, ListView):
     model = Lesson
-    
+    count_hit = True
+
     def get_queryset(self):
         queryset = Lesson.published_objects.all()
         return queryset
-    
 
 
-class LessonDetailView(DetailView):
+class LessonDetailView(HitCountDetailView):
     model = Lesson
+    count_hit = True
